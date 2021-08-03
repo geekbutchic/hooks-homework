a75db7114efece86d2924de3aabb3000fa0cc68e
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-08-02 23:33:38 -0400

    update function to add submit button with onSubmit functionality

--- src/App-1.js
+++ src/App-1.js
@@ -6,32 +6,41 @@ function App() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log(username);
+    console.log(email);
+    console.log(password);
+  }
 
   return (
-    <div className="App border-4 border-indigo-400 rounded-md">
-      <form>
+    <div className="App border-4 border-indigo-400 rounded-md bg-auto">
+      <form onSubmit={handleOnSubmit}>
         <input
-        type="text"
-        placeholder="USERNAME" 
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        autoFocus
+          type="text"
+          placeholder="USERNAME"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          autoFocus
         />
         <br />
         <input
-        type="text"
-        placeholder="EMAIL"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}   
+          type="email"
+          placeholder="EMAIL"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <br />
-        <input 
-        type="email"
-        placeholder="PASSWORD"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}  
+        <input
+          type="password"
+          placeholder="PASSWORD"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
+          <div className="bg-red-500 hover:bg-red-500 rounded-md">
+          <button type="submit">SUBMIT</button>
+        </div>
       </form>
     </div>
   );
