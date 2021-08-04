459e2745d69e9f8ae0740009239b69252803a2c9
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2021-08-04 14:44:03 -0400

    adds object version of hooks for submit, email, password

--- src/App-1.js
+++ src/App-1.js
@@ -1,6 +1,5 @@
 //OBJECT VERSION
 import React, { useState } from "react";
-
 import "./App.css";
 
 function CustomHooksObjectsFormat(initialState) {
@@ -9,22 +8,67 @@ function CustomHooksObjectsFormat(initialState) {
   function onChange(e) {
     setValue(e.target.value);
   }
+  function clearInput() {
+    setValue("");
+  }
 
-  return { value, onChange };
+  return { value, onChange, clearInput };
 }
 
 function App() {
-  const firstName = CustomHooksObjectsFormat("");
-  console.log(firstName);
+  const {
+    value: userName,
+    onChange: userNameOnChange,
+    clearInput: clearUserNameInput,
+  } = CustomHooksObjectsFormat("");
+
+  const {
+    value: email,
+    onChange: emailOnChange,
+    clearInput: clearEmailInput,
+  } = CustomHooksObjectsFormat("");
+
+  const {
+    value: password,
+    onChange: passwordOnChange,
+    clearInput: clearPasswordInput,
+  } = CustomHooksObjectsFormat("");
+
+  function handleOnSubmit (e) {
+    e.preventDefault();
+    clearUserNameInput();
+    clearEmailInput();
+    clearPasswordInput();
+
+    console.log("USER NAME: ", userName);
+    console.log("EMAIL: ", email);
+    console.log("PASSWORD: ", password);
+  }
 
   return (
     <div className="App border-4 border-indigo-400 rounded-md bg-auto">
-      <form>
-        <input type="text" placeholder="USERNAME" autoFocus />
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="USERNAME"
+          value={userName}
+          onChange={userNameOnChange}
+          autoFocus
+        />
         <br />
-        <input type="email" placeholder="EMAIL" />
+        <input
+          type="email"
+          placeholder="EMAIL"
+          value={email}
+          onChange={emailOnChange}
+        />
         <br />
-        <input type="password" placeholder="PASSWORD" />
+        <input
+          type="password"
+          placeholder="PASSWORD"
+          value={password}
+          onChange={passwordOnChange}
+        />
         <div className="bg-red-500 hover:bg-red-500 rounded-md">
           <button type="submit">SUBMIT</button>
         </div>
